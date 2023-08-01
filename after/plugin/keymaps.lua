cf118c9d9ff91f94600c42a0efb2e7dd61131b34
Simon Stipcich <simon.stipcich@derivco.co.za>
2023-08-01 17:13:51 +0200

    feat:after

---
+++ after/plugin/keymaps.lua
@@ -0,0 +1,43 @@
+local keymap = vim.api.nvim_set_keymap
+local default_opts = { noremap = true, silent = true }
+local expr_opts = { noremap = true, expr = true, silent = true }
+
+-- Visual line wraps
+keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", expr_opts)
+keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", expr_opts)
+
+-- Better indent
+keymap("v", "<", "<gv", default_opts)
+keymap("v", ">", ">gv", default_opts)
+
+-- Paste over currently selected text without yanking it
+keymap("v", "p", '"_dP', default_opts)
+
+-- Switch buffer
+-- keymap("n", "<S-h>", ":BufferLineCyclePrev<CR>", default_opts)
+-- keymap("n", "<S-l>", ":BufferLineCycleNext<CR>", default_opts)
+keymap("n", "<S-h>", ":bn<CR>", default_opts)
+keymap("n", "<S-l>", ":bp<CR>", default_opts)
+
+keymap("n", "<C-h>", "<C-w>h", default_opts)
+keymap("n", "<C-l>", "<C-w>l", default_opts)
+keymap("n", "<C-k>", "<C-w>k", default_opts)
+keymap("n", "<C-j>", "<C-w>j", default_opts)
+
+-- Cancel search highlighting with ESC
+keymap("n", "<ESC>", ":nohlsearch<Bar>:echo<CR>", default_opts)
+
+-- Move selected line / block of text in visual mode
+keymap("x", "K", ":move '<-2<CR>gv-gv", default_opts)
+keymap("x", "J", ":move '>+1<CR>gv-gv", default_opts)
+
+-- Resizing panes
+keymap("n", "<Left>", ":vertical resize +1<CR>", default_opts)
+keymap("n", "<Right>", ":vertical resize -1<CR>", default_opts)
+keymap("n", "<Up>", ":resize +1<CR>", default_opts)
+keymap("n", "<Down>", ":resize -1<CR>", default_opts)
+
+-- Insert blank line
+keymap("n", "]<Space>", "o<Esc>", default_opts)
+keymap("n", "[<Space>", "O<Esc>", default_opts)
+
